31d85a51a6b730f4f410a142ce9dd50211a6ea15
Sungjin Kim <sungjin3@hawaii.edu>
2023-09-21 16:13:32 -1000

    Update Chap11_Factor Analysis.R

--- Chap11_Factor Analysis.R
+++ Chap11_Factor Analysis.R
@@ -3,66 +3,62 @@
 ###################
 
 ## Install Packages (if needed)
-install.packages("nFactors")
-install.packages("dplyr")
 install.packages("GPArotation")
 install.packages("gplots")
 install.packages("RColorBrewer")
 
 ## Load Packages and Set Seed
-library(nFactors)
-library(dplyr)
+library(tidyverse)
 library(GPArotation)
 library(gplots)
 library(RColorBrewer)
+library(readr)
 set.seed(1)
+setwd("G:/My Drive/Teaching/Marketing Analytics/Chapter Examples")
 
 ## Read in Factor Analysis data
-retailer_survey <- read.csv(file.choose()) ## Choose retailer_survey.csv file
 
-summary(retailer_survey)
+retailer_survey <- read_csv("Chapter 11/retailer_survey.csv")
+str(retailer_survey)
+
 ## Look at Descriptive Statistics
 
 retailer.factor.mean = 
-retailer_survey %>% group_by(Retailer) %>% summarise(across(where(is.numeric), mean))
-
+  retailer_survey %>% group_by(Retailer) %>% summarise(across(where(is.numeric), mean))
+retailer.factor.mean
 
 ## Determine Number of Factors
-retailer_factors <- retailer_survey %>% select(-Retailer)
+retailer_factors <- retailer_survey %>% select(-Retailer) #need to exclude "Retailer" since it is categorical identifier of retailers
 eigen(cor(retailer_factors))$values
-
+retailer_factors
 ## Run Factor Analysis with 4 factors
-factanal(retailer_factors, factors = 4)
+factanal(x = retailer_factors, factors = 4)
 
 ## Run Factor Analysis with 4 factors and oblique rotation
-retailer.fa <- factanal(retailer_factors, factors = 4, rotation="oblimin", scores="Bartlett")
+retailer.fa <- factanal(x = retailer_factors, factors = 4, rotation="oblimin",scores = "Bartlett")
 retailer.fa
-
+?factanal
 ## Print a Heatmap of Factor Loadings
 heatmap.2(retailer.fa$loadings, col=brewer.pal(9, "Reds"), trace="none", key=FALSE, dend="none",
 	Colv=FALSE, cexCol = 1, margins=c(12,8), main="Factor Loadings from Survey")
 
 ## Aggregate Factor Scores by Retailer
 retailer.scores <- data.frame(retailer.fa$scores)
+retailer.scores
 retailer.scores$retailer <- retailer_survey$Retailer
-retailer.fa.mean <- aggregate(. ~ retailer, data=retailer.scores, mean)
-rownames(retailer.fa.mean) <- retailer.fa.mean[,1]
-retailer.fa.mean <- select(retailer.fa.mean, -retailer)
-names(retailer.fa.mean) <- c("Innovative", "High Quality", "Loyalty", "Good Value") 
+retailer.fa.mean = retailer.scores %>% group_by(retailer) %>% summarise(across(where(is.numeric), mean))
+retailer.fa.mean = column_to_rownames(retailer.fa.mean, var = "retailer") 
+retailer.fa.mean = 
+retailer.fa.mean %>% rename(Innovative = Factor1,
+                            Quality = Factor2,
+                            Loyalty = Factor3,
+                            Value = Factor4)
+
 retailer.fa.mean
 
-## Print a Heatmap of Retailer Scores
-heatmap.2(as.matrix(retailer.fa.mean), col=brewer.pal(9, "Blues"), trace="none", key=FALSE, dend="none",
-	Colv=FALSE, cexCol = 1, margins=c(12,8), main="Factor Score by Retailer")
-
-
+## Print a Heatmap of Retailer Scores; Feel free to play around the options
 heatmap.2(as.matrix(retailer.fa.mean), col=brewer.pal(9, "Blues"), 
-          trace="none",
-          key=FALSE,
-          dend="none",
-          Colv=FALSE,
-          cexCol = 1,
-          margins=c(12,8),
+          trace="none", key=FALSE, dend="none",
+          Colv=FALSE, cexCol = 1, margins=c(12,8),
           main="Factor Score by Retailer")
-
 
